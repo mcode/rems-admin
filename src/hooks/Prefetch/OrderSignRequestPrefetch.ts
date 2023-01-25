6aa3b82496727e96f995270d2c59fb868b161d67
kghoreshi <kghoreshi@mitre.org>
2023-01-25 16:05:29 -0500

    some linting

--- src/hooks/Prefetch/OrderSignRequestPrefetch.ts
+++ src/hooks/Prefetch/OrderSignRequestPrefetch.ts
@@ -1,4 +1,4 @@
-import { MedicationRequest, Patient, Practitioner } from 'fhir/r4';
+import { Patient, Practitioner } from 'fhir/r4';
 import RequestPrefetch from './RequestPrefetch';
 export default interface OrderSignRequestPrefetch extends RequestPrefetch {
   patient?: Patient;
