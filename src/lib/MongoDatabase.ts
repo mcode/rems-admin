99599f00478c1fd4b362d02b6f60f85f309ec1ed
Keeyan <keeyan.ghoreshi@uconn.edu>
2023-03-15 16:40:05 -0400

    connect to mongoose (#55)
    * connect to mongoose
    
    * linting
    
    ---------
    
    Co-authored-by: kghoreshi <kghoreshi@mitre.org>

--- src/lib/MongoDatabase.ts
+++ src/lib/MongoDatabase.ts
@@ -1,6 +1,6 @@
 import { Database } from './Database';
 import * as mongoDB from 'mongodb';
-
+import mongoose from 'mongoose';
 export class MongoDatabase extends Database {
   options: any;
   db_name: string;
@@ -16,8 +16,9 @@ export class MongoDatabase extends Database {
     new Promise(resolve => {
       // Connect to mongo
       console.log('MongoDatabase connect: ' + this.location);
-      this.client = new mongoDB.MongoClient(this.location);
+      const dbString = `${this.location}/${this.db_name}`;
+      this.client = new mongoose.mongo.MongoClient(dbString);
       this.database = this.client.db(this.db_name);
-      return resolve(this.client.connect());
+      return resolve(mongoose.connect(dbString));
     });
 }
