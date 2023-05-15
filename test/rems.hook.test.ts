eeccae872690ec9fe1e821d910027a44e94e0308
Sahil Malhotra <smalhotra@mitre.org>
2023-05-15 13:36:26 -0400

    remove rems test hook

--- test/rems.hook.test.ts
+++ test/rems.hook.test.ts
@@ -2,24 +2,24 @@
 // import getREMSHook from '../src/hooks/rems.hook';
 // import { expect } from 'chai';
 
-// describe.skip('hook: test rems', () => {
-//   it('should have definition and handler', () => {
-//     const prefetch = {
-//       patient: 'Patient/{{context.patientId}}',
-//       practitioner: 'Practitioner/{{context.userId}}'
-//     };
-//     const expectedDefinition = new OrderSign(
-//       'rems-order-sign',
-//       'order-sign',
-//       'REMS Requirement Lookup',
-//       'REMS Requirement Lookup',
-//       prefetch
-//     );
+describe.skip('hook: test rems', () => {
+  it('should have definition and handler', () => {
+    const prefetch = {
+      patient: 'Patient/{{context.patientId}}',
+      practitioner: 'Practitioner/{{context.userId}}'
+    };
+    // const expectedDefinition = new OrderSign(
+    //   'rems-order-sign',
+    //   'order-sign',
+    //   'REMS Requirement Lookup',
+    //   'REMS Requirement Lookup',
+    //   prefetch
+    // );
 
-//     expect(getREMSHook).to.haveOwnProperty('definition');
-//     expect(getREMSHook).to.haveOwnProperty('handler');
+    // expect(getREMSHook).to.haveOwnProperty('definition');
+    // expect(getREMSHook).to.haveOwnProperty('handler');
 
-//     expect(getREMSHook.definition).to.deep.equal(expectedDefinition);
-//     expect(getREMSHook.handler).to.instanceOf(Function);
-//   });
-// });
+    // expect(getREMSHook.definition).to.deep.equal(expectedDefinition);
+    // expect(getREMSHook.handler).to.instanceOf(Function);
+  });
+});
