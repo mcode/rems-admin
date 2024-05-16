7fc7e0c51e5ed3d5d1357ebf0307196f4c90e679
Patrick LaRocque <plarocque@mitre.org>
2024-05-16 00:12:43 -0400

    support encounter start

--- src/hooks/rems.encounterstart.ts
+++ src/hooks/rems.encounterstart.ts
@@ -1,9 +1,9 @@
-import { PatientViewHook, SupportedHooks } from '../rems-cds-hooks/resources/HookTypes';
+import { EncounterStartHook, SupportedHooks } from '../rems-cds-hooks/resources/HookTypes';
 import { ServicePrefetch, CdsService } from '../rems-cds-hooks/resources/CdsService';
 import { handleCardEncounter, handleHook } from './hookResources';
 
 interface TypedRequestBody extends Express.Request {
-  body: PatientViewHook;
+  body: EncounterStartHook;
 }
 
 const hookPrefetch: ServicePrefetch = {
@@ -13,8 +13,8 @@ const hookPrefetch: ServicePrefetch = {
     'MedicationRequest?subject={{context.patientId}}&_include=MedicationRequest:medication'
 };
 const definition: CdsService = {
-  id: 'rems-patient-view',
-  hook: SupportedHooks.PATIENT_VIEW,
+  id: 'rems-encounter-start',
+  hook: SupportedHooks.ENCOUNTER_START,
   title: 'REMS Requirement Lookup',
   description: 'REMS Requirement Lookup',
   prefetch: hookPrefetch
@@ -25,7 +25,7 @@ const source = {
 };
 
 const handler = (req: TypedRequestBody, res: any) => {
-  console.log('REMS patient-view hook');
+  console.log('REMS encounter-start hook');
   const contextRequest = undefined;
   handleHook(req, res, hookPrefetch, contextRequest, handleCardEncounter);
 };
