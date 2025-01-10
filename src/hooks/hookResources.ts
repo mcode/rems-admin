c38e684da2c67782706affc5909ad490a252402c
Patrick LaRocque <plarocque@mitre.org>
2025-01-10 09:56:01 -0500

    fix lint and prettier errors

--- src/hooks/hookResources.ts
+++ src/hooks/hookResources.ts
@@ -297,20 +297,18 @@ export function createSmartLink(
   appContext: string | null,
   request: MedicationRequest | undefined
 ) {
-  var order;
+  let order;
   if (config.general.fullResourceInAppContext) {
     order = JSON.stringify(request);
   } else {
-    order = request?.resourceType + '/' + request?.id; 
+    order = request?.resourceType + '/' + request?.id;
   }
 
   const newLink: Link = {
     label: requirementName + ' Form',
     url: new URL(config.smart.endpoint),
     type: 'smart',
-    appContext: `${appContext}&order=${order}&coverage=${
-      request?.insurance?.[0].reference
-    }`
+    appContext: `${appContext}&order=${order}&coverage=${request?.insurance?.[0].reference}`
   };
   return newLink;
 }
